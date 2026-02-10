fc551d8dd129b85c2c5540c1839c222561e0b711
madhumithamasai200617-netizen <madhumitha.masai200617@gmail.com>
2026-02-10 19:40:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-<script>
-window.onload = () => {
-    alert("Happy Valentine’s Day, my love ❤️");
-};
-</script>
+function openProposal() {
+  window.open("proposal.html", "_blank");
+}
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.innerHTML = "❤️";
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = Math.random() * 3 + 4 + "s";
+
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 6000);
+}
+
+setInterval(createHeart, 300);
