ddebab8bcd5b1b7c1f7e03b14195aad25fcaf0be
madhumithamasai200617-netizen <madhumitha.masai200617@gmail.com>
2026-02-10 20:06:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,5 +16,36 @@ function createHeart() {
     heart.remove();
   }, 6000);
 }
+// Notes appear one by one
+const notes = document.querySelectorAll(".note");
+
+notes.forEach((note, index) => {
+  note.style.animationDelay = `${index * 0.6}s`;
+});
+
+// Heart burst on click
+notes.forEach(note => {
+  note.addEventListener("click", (e) => {
+    for (let i = 0; i < 10; i++) {
+      const heart = document.createElement("span");
+      heart.classList.add("burst-heart");
+      heart.innerHTML = "❤️";
+
+      const x = (Math.random() - 0.5) * 100 + "px";
+      const y = (Math.random() - 0.5) * 100 + "px";
+
+      heart.style.setProperty("--x", x);
+      heart.style.setProperty("--y", y);
+
+      heart.style.left = e.pageX + "px";
+      heart.style.top = e.pageY + "px";
+
+      document.body.appendChild(heart);
+
+      setTimeout(() => heart.remove(), 1000);
+    }
+  });
+});
+
 
 setInterval(createHeart, 300);
